d4c13aae73ae320a3239cd010adeaebfef166906
Daniel Burnley <dan@danops.uk>
2018-11-16 09:16:21 +0000

    Set api url environment variable on deploy

--- bin/deploy-dark.sh
+++ bin/deploy-dark.sh
@@ -7,6 +7,12 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
+API_URL=$API_URL_STAGING
+
+if [ "$1" -eq "production" ]; then
+  API_URL=$API_URL_PRODUCTION
+fi
+
 curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
 ./cf api https://api.cloud.service.gov.uk
 ./cf auth ${CF_USER} ${CF_PASSWORD}
@@ -16,3 +22,4 @@ curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=
 ./cf push -f deploy-manifests/${1}-dark.yml
 ./cf set-env asset-register-frontend-${1}-dark circle_commit ${CIRCLE_SHA1}
 ./cf set-env asset-register-frontend-${1}-dark REACT_APP_SENTRY_DSN ${SENTRY_DSN}
+./cf set-env asset-register-frontend-${1}-dark REACT_APP_ASSET_REGISTER_API_URL ${API_URL}
