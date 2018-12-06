a14388350a72e3cff7b5e18ed3dab5a901531212
Daniel Burnley <dan@danops.uk>
2018-12-06 17:43:11 +0000

    Update stories

--- src/Components/AssetsProvider/stories.js
+++ src/Components/AssetsProvider/stories.js
@@ -3,14 +3,23 @@ import AssetsProvider from ".";
 
 import { storiesOf } from "@storybook/react";
 
+const historyStub = {
+  storeSearch: route => {
+    console.log(route);
+  }
+};
+
 storiesOf("AssetsProvider", module).add("Default", () => {
   let searchAssetsStub = { execute: () => [{ cat: "meow" }] };
 
   return (
-    <AssetsProvider searchAssets={searchAssetsStub}>
+    <AssetsProvider history={historyStub} searchAssets={searchAssetsStub}>
       {({ onSearch, assets }) => (
         <div>
-          <button type="button" onClick={onSearch}>
+          <button
+            type="button"
+            onClick={() => onSearch({ params: "Searched" })}
+          >
             Click me to search the assets
           </button>
           <p>Asset count = {assets.length}</p>
