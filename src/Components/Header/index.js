f311f479cdf0e9b78b6dc68c6abf16b65945c2fd
Daniel Burnley <dan@danops.uk>
2019-01-29 11:54:39 +0000

    Update user flow through application

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "govuk-frontend/all.scss";
 
-export default (props) => (
+export default props => (
   <header className="govuk-header " role="banner" data-module="header">
     <div className="govuk-header__container govuk-width-container">
       <div className="govuk-header__logo">
-        <props.linkComponent to="/" className="govuk-header__link govuk-header__link--homepage">
+        <props.linkComponent
+          to="/"
+          className="govuk-header__link govuk-header__link--homepage"
+        >
           <span className="govuk-header__logotype">
-            <span className="govuk-header__logotype-text">Homes Equity</span>
+            <span className="govuk-header__logotype-text">Homes England</span>
           </span>
         </props.linkComponent>
       </div>
       <div className="govuk-header__content">
-        <props.linkComponent to="/" className="govuk-header__link govuk-header__link--service-name">
-          Asset Register
+        <props.linkComponent
+          to="/"
+          className="govuk-header__link govuk-header__link--service-name"
+        >
+          Homes Equity Asset Register
         </props.linkComponent>
       </div>
     </div>
